0c4751a51f7e29b1ba4ef476c02f843845fe1a35
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-12 11:20:51 -0300

    Double tap gesture and animations

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -10,13 +10,48 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTY
     
+    @State private var isAnimatiing: Bool = false
+    @State private var imageScale: CGFloat = 1
+    
     // MARK: - FUNCTION
     
     // MARK: - CONTENT
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimatiing ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .animation(.linear(duration: 1), value: isAnimatiing)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimatiing = true
+                }
+            })
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
@@ -25,6 +60,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
             .previewDevice("iPhone 14")
     }
 }
