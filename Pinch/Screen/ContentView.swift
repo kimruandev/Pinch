44d8a38ce902ccc1d3057748c2add4d379a0afcf
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-12 10:59:33 -0300

    Project settings

---
+++ Pinch/Screen/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Kim Ruan on 12/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+// MARK: - PREVIEW
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 14")
+    }
+}
