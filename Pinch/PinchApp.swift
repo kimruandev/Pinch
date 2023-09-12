80f4a81fef8c92b9117197c02c855b6f62c4593e
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-12 10:42:23 -0300

    Hello World!

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Kim Ruan on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
